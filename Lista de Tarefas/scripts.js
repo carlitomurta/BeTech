bd330c2c1ed6a34bf9c52a4829cc597f0504e006
Carlito Murta <cmurta@lean-tech.io>
2022-06-30 20:30:58 -0300

    Finalizaçao da lista de tarefas

--- Lista de Tarefas/scripts.js
+++ Lista de Tarefas/scripts.js
@@ -1,17 +1,22 @@
 const TAREFAS_KEY = "tarefas";
-const tarefas = JSON.parse(localStorage.getItem(TAREFAS_KEY));
+const conteudo = document.getElementById("conteudo");
+const lista = document.getElementById("lista");
+let tarefas = JSON.parse(localStorage.getItem(TAREFAS_KEY));
 
 const iniciar = () => {
-  construir();
+  if (tarefas) {
+    construir();
+  } else {
+    notFound();
+  }
 };
 
 const construir = () => {
-  // const conteudoEl = document.getElementById("conteudo");
-  const lista = document.getElementById("lista");
+  console.log(tarefas);
   tarefas.forEach((item) => {
     lista.innerHTML += `
     <li class="item">
-    <label id="${item.id}">
+    <label id="${item.id}" class="${item.checked ? "checked_label" : ""}">
       <input id="${item.id}" type="checkbox" ${
       item.checked ? "checked" : ""
     } onclick="checkItem(this)" />
@@ -32,17 +37,56 @@ const construir = () => {
   });
 };
 
+const notFound = () => {
+  const paragraph = document.createElement("p");
+  paragraph.textContent = "Nao existem tarefas para hoje :D.";
+  conteudo.append(paragraph);
+};
+
+const gerarTarefa = (item) => {
+  lista.innerHTML += `
+    <li class="item">
+    <label id="${item.id}">
+      <input id="${item.id}" type="checkbox" ${
+    item.checked ? "checked" : ""
+  } onclick="checkItem(this)" />
+      ${item.nome}
+    </label>
+  </li>`;
+  if (tarefas) {
+    const p = document.querySelector("p");
+    conteudo.removeChild(p);
+  }
+};
+
+const adicionarTarefa = () => {
+  const nome = prompt("Adicione um titulo para sua tarefa!", "");
+  if (nome) {
+    const item = {
+      id: tarefas ? tarefas.length + 1 : 1,
+      nome,
+      checked: false,
+    };
+    tarefas = tarefas ? [...tarefas, item] : [item];
+    localStorage.setItem(TAREFAS_KEY, JSON.stringify(tarefas));
+    gerarTarefa(item);
+  } else {
+    // Nao faça nada
+  }
+};
+
 const checkItem = (el) => {
+  const tarefa = tarefas.find((item) => item.id == el.id);
+  const item = tarefas[tarefas.indexOf(tarefa)];
   const labelEl = document.getElementById(el.id);
   if (!el.checked) {
     labelEl.classList.remove("checked_label");
+    item.checked = false;
   } else {
-    localStorage.setItem(
-      TAREFAS_KEY,
-      JSON.stringify({ id: el.id, value: el.checked })
-    );
     labelEl.classList.add("checked_label");
+    item.checked = true;
   }
+  localStorage.setItem(TAREFAS_KEY, JSON.stringify(tarefas));
 };
 
 // Local Storage - Armazenamento local - Armazenamento do Browser
@@ -52,3 +96,16 @@ const checkItem = (el) => {
 // - Dados trafegaveis
 
 // Cookies - Armazenamento de trafego - API's - SEO
+
+// <div class="categorias">
+//   $
+//   {(() => {
+//     let string = "";
+//     if (item.categorias) {
+//       item.categorias.forEach((categoria) => {
+//         string += `<div class="pilula">${categoria}</div>`;
+//       });
+//     }
+//     return string;
+//   })()}
+// </div>;
